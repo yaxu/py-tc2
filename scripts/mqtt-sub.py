826b23994e77efc5d39dc20e29a49ee9bfad25c5
alex <alex@slab.org>
2024-11-11 15:39:07 +0100

    more scripts

---
+++ scripts/mqtt-sub.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("/pattern")
+    client.subscribe("/hello")
+    client.subscribe("/pattern/json")
+    client.subscribe("/tc2/footswitch")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.username_pw_set(username="tue",password="runningwithscissors")
+#mqttc.tls_set()
+mqttc.connect("slab.org", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+run = True
+while run:
+    rc = mqttc.loop(timeout=1.0)
+    if rc != 0:
+        print("mqtt error")
+        run = False
---
+++ scripts/slowweaving.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+import paho.mqtt.client as mqtt
+import json, math
+
+def bjorklund(steps, pulses):
+    steps = int(steps)
+    pulses = int(pulses)
+    if pulses > steps:
+        raise ValueError    
+    pattern = []    
+    counts = []
+    remainders = []
+    divisor = steps - pulses
+    remainders.append(pulses)
+    level = 0
+    while True:
+        counts.append(divisor // remainders[level])
+        remainders.append(divisor % remainders[level])
+        divisor = remainders[level]
+        level = level + 1
+        if remainders[level] <= 1:
+            break
+    counts.append(divisor)
+    
+    def build(level):
+        if level == -1:
+            pattern.append(0)
+        elif level == -2:
+            pattern.append(1)         
+        else:
+            for i in range(0, counts[level]):
+                build(level - 1)
+            if remainders[level] != 0:
+                build(level - 2)
+    
+    build(level)
+    i = pattern.index(1)
+    pattern = pattern[i:] + pattern[0:i]
+    return pattern
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    client.subscribe("/tc2/footswitch")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    if msg.topic == '/tc2/footswitch':
+        delta = json.loads(msg.payload)
+        pulses = min(int(math.log(delta) * 10),32)
+        steps = 32
+        pattern = "".join(bjorklund(steps,pulses))
+        print("sending", pattern)
+        client.publish("/pattern", pattern)
+
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.username_pw_set(username="tue",password="runningwithscissors")
+#mqttc.tls_set()
+mqttc.connect("slab.org", 1883, 60)
+mqttc.publish("/pattern", "0")
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+run = True
+while run:
+    rc = mqttc.loop(timeout=1.0)
+    if rc != 0:
+        print("mqtt error")
+        run = False
