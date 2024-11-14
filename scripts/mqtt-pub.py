b99fabf66d5a175a5a36f8fbb8731ceba1807c26
alex <alex@slab.org>
2024-11-14 16:34:13 +0100

    end of residency

--- scripts/mqtt-pub.py
+++ scripts/mqtt-pub.py
@@ -4,5 +4,5 @@ import paho.mqtt.publish as publish
 import json
 
 # publish.single("/pattern", "1001101010", hostname="slab.org", auth={"username":"tue","password":"runningwithscissors"})
-publish.single("/tc2/footswitch", "1.2", hostname="slab.org", auth={"username":"tue","password":"runningwithscissors"})
+publish.single("/tc2/footswitch", "6", hostname="slab.org", auth={"username":"tue","password":"runningwithscissors"})
 
